63846c200238da5ff35b2f0798cc351706a098f0
Evgeny Zinovyev <evgeny.zinovyev@eurowings.com>
2020-04-30 22:18:04 +0200

    add flow to select departure station

--- app.py
+++ app.py
@@ -4,9 +4,9 @@
 import logging
 
 import telegram
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-from data import get_flights_by_day, fetch_weeks
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
+from data import get_flights_by_day, fetch_weeks, fetch_stations
 from reply import create_replay
 from values import TELEGRAM_BOT_TOKEN
 
@@ -17,6 +17,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 ORIGIN, DESTINATION, WEEK, FLIGHTS = range(4)
+ORIGIN_QUERY, ORIGIN_RESULT, ORIGIN_CONFIRM, DESTINATION_QUERY = range(4)
+
+DEPARTURE_CONV_BTN_DEST, DEPARTURE_CONV_BTN_CHANGE = range(2)
 
 
 def help(update, context):
@@ -29,12 +32,59 @@ Commands:
 
 def start(update, context):
     update.message.reply_text(
-"""Welcome to Eurowings Telegram Info Bot\. You can lookup for the following information
+        """Welcome to Eurowings Telegram Info Bot\. You can lookup for the following information
 *\/weekplanner* 
 To display all possible services use
 *\/help* commnand
         """, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
+
+def select_origin(update, context):
+    reply_keyboard = [['CGN', 'HAM', 'STR']]
+    update.message.reply_text("Enter the TLC or a airport name(at least three letters)",
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return ORIGIN_QUERY
+
+
+def find_origin(update, context):
+    origin = update.message.text
+    stations = fetch_stations(origin)
+    reply_keyboard = []
+    for station in stations:
+        # reply_keyboard.append([f"{station['label']}"])
+        reply_keyboard.append([InlineKeyboardButton(text=f"{station['label']}", callback_data=station['value'])])
+    update.message.reply_text(f"Select origin",
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True,
+                                                               one_time_keyboard=True))
+    return ORIGIN_RESULT
+
+
+def confirm_origin(update, context):
+    chosen_origin = update.message.text
+    context.user_data['departure'] = chosen_origin
+
+    # show user next steps
+    reply_keyboard = [[InlineKeyboardButton('Change departure', callback_data=str(DEPARTURE_CONV_BTN_CHANGE))],
+                      [InlineKeyboardButton('Select destination', callback_data=str(DEPARTURE_CONV_BTN_DEST))]]
+
+    update.message.reply_text(f"Your departure station is {chosen_origin}. What you would like to do next?",
+                              reply_markup=InlineKeyboardMarkup(reply_keyboard))
+
+    return DESTINATION_QUERY
+
+
+def departure_buttons(update, context):
+    query = update.callback_query
+    query.answer()
+
+    if query.data == str(DEPARTURE_CONV_BTN_DEST):
+        query.edit_message_text("Enter the departure station TLC or a airport name(at least three letters)")
+    else:
+        query.edit_message_text("not implemented now")
+
+
+def find_destination(update, context):
+    update.message.reply_text("hey")
 
 def week_planner(update, context):
     reply_keyboard = [['CGN', 'HAM', 'STR']]
@@ -46,19 +96,6 @@ Then let us start with the origin station you want to fly from
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
     return ORIGIN
-
-
-def echo(update, context):
-    message = update.edited_message if update.edited_message else update.message
-    # logger.info(message.text)
-
-    # context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-    # markup = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton(text="Heyyy-%d" % getFakeId())]])
-    # message.reply_text("""hey""", parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=markup)
-    flights = get_flights_by_day("CGN", "BER", "2020-11-16", "2020-11-22")
-    for flight in flights:
-        message.reply_text(create_replay(flight))
 
 
 def origin(update, context):
@@ -132,6 +169,18 @@ def init():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    conv_select_route = ConversationHandler(
+        entry_points=[CommandHandler('origin', select_origin)],
+
+        states={
+            ORIGIN_QUERY: [MessageHandler(Filters.text, find_origin)],
+            ORIGIN_RESULT: [MessageHandler(Filters.text, confirm_origin)],
+            DESTINATION_QUERY: [MessageHandler(Filters.text, find_destination)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('weekplanner', week_planner)],
 
@@ -150,10 +199,13 @@ def init():
     )
 
     dp.add_handler(conv_handler)
+    dp.add_handler(conv_select_route)
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(CallbackQueryHandler(departure_buttons))
 
     # on noncommand i.e message - echo the message on Telegram
     # dp.add_handler(MessageHandler(Filters.text, echo))
